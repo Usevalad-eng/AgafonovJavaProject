2ba390902f29781554cbb4d4b4f9b356066e860d
Usevalad <avm012@gmail.com>
2025-04-28 05:39:14 -0700

    25apr2025

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -24,7 +24,7 @@ public class MainClass {
 
         System.out.println("----------------");
 
-        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Array array1 = new Array(array);
         array1.getArray();
 
